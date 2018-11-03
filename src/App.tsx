59723fff97c7f4af74dc1422b1787a6764a7ac23
Danila_Leontyev <Danila.leontyev.web@gmail.com>
2018-11-03 01:22:11 -0700

    добавил вибор страниц, разобрался с появлением кнопок

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
+import './component/Greetings';
+import Greetings from './component/Greetings';
+import BinarySearchQuestion from './component/BinarySearchQuestion';
 
 interface IAppState {
   questionNumber: number;
 }
 
-const appHeader = (
-  <header className="appHeader">
-    <img className="appHeader--logo" src={logo} />
-    <button className="button">Далее</button>
-  </header>
-);
-
-const appFooter = (
-  <footer className="appFooter">
-    <button className="appFooter button">Предыдущее</button>
-    <button className="appFooter button">Далее</button>
-  </footer>
-);
 class App extends Component<any, IAppState> {
   constructor(props: any) {
     super(props);
@@ -27,11 +17,77 @@ class App extends Component<any, IAppState> {
     };
   }
 
+  public nextPage = (e: any): void => {
+    if (this.state.questionNumber < 5) {
+      this.setState(state => {
+        return {
+          questionNumber: state.questionNumber + 1
+        };
+      });
+    }
+  };
+
+  public prevPage = (e: any): void => {
+    if (this.state.questionNumber > 0) {
+      this.setState(state => {
+        return {
+          questionNumber: state.questionNumber - 1
+        };
+      });
+    }
+  };
+
+  public choosePage = (): JSX.Element => {
+    switch (this.state.questionNumber) {
+      case 0: {
+        return <Greetings />;
+      }
+
+      case 1: {
+        return <BinarySearchQuestion />;
+      }
+
+      case 2: {
+        return <span>page2</span>;
+      }
+
+      case 3: {
+        return <span>page3</span>;
+      }
+
+      case 4: {
+        return <span>page4</span>;
+      }
+
+      case 5: {
+        return <span>page5</span>;
+      }
+
+      default: {
+        return <span>error</span>;
+      }
+    }
+  };
+
   render() {
+    const { questionNumber } = this.state;
     return (
       <div className="App">
-        {appHeader}
-        {appFooter}
+        <header className="appHeader">
+          <img className="appHeader--logo" src={logo} />
+          <div className="appHeader--buttonContainer">
+            <button
+              className={`button ${questionNumber ? '' : 'hidden'}`}
+              onClick={this.prevPage}
+            >
+              Назад
+            </button>
+            <button className="button" onClick={this.nextPage}>
+              Далее
+            </button>
+          </div>
+        </header>
+        {this.choosePage()}
       </div>
     );
   }
---
+++ src/component/BinarySearchQuestion.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class BinarySearchQuestion extends Component<any> {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <main className="binarySearchQuestion">
+        <div>Binary</div>
+      </main>
+    );
+  }
+}
+
+export default BinarySearchQuestion;
