18dc15534f31beeb9176ceaf98bb720a62c2a561
Danila_Leontyev <Danila.leontyev.web@gmail.com>
2018-11-03 02:03:21 -0700

    добавил dnd

--- src/component/BinarySearchQuestion.tsx
+++ src/component/BinarySearchQuestion.tsx
@@ -1,14 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component, ReactElement } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 class BinarySearchQuestion extends Component<any> {
   constructor(props: any) {
     super(props);
   }
 
+  onDragEnd = (result: any) => {
+    if (!result.destination) {
+      return;
+    }
+  };
+
   render() {
     return (
       <main className="binarySearchQuestion">
-        <div>Binary</div>
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable">
+            {(provider, snapshot): any => (
+              <div ref={provider.innerRef}>
+                <Draggable key="1" draggableId="1" index={1}>
+                  {(provider, snapshot): any => (
+                    <div
+                      ref={provider.innerRef}
+                      {...provider.draggableProps}
+                      {...provider.dragHandleProps}
+                    >
+                      Hello world
+                    </div>
+                  )}
+                </Draggable>
+
+                <Draggable key="2" draggableId="2" index={2}>
+                  {(provider, snapshot): any => (
+                    <div
+                      ref={provider.innerRef}
+                      {...provider.draggableProps}
+                      {...provider.dragHandleProps}
+                    >
+                      Hello world
+                    </div>
+                  )}
+                </Draggable>
+
+                {provider.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </main>
     );
   }
