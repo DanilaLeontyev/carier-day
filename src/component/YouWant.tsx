856fe7fa84e18fb67e8a5680e68494c5174a5ad7
Danila_Leontyev <Danila.leontyev.web@gmail.com>
2018-11-06 18:56:50 +0500

    realise choose prof

--- src/component/YouWant.tsx
+++ src/component/YouWant.tsx
@@ -2,11 +2,34 @@ import React, { Component } from 'react';
 import './YouWant.css';
 import lamp from '../assets/lamp.png';
 
-class YouWant extends Component<any> {
-  chooseProf = (e: any) => {
-    e.target.className += ' button__selected';
+interface IYouWantState {
+  choosenProf: string;
+}
+class YouWant extends Component<any, IYouWantState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      choosenProf: ''
+    };
+  }
 
-    console.log(e.target.className);
+  chooseProf = (e: any) => {
+    const choosenProf = e.target.textContent;
+    const profButtons = this.getAllButton();
+    for (let i = 0; i < profButtons.length; i++) {
+      if (profButtons[i].classList.contains('button__selected')) {
+        profButtons[i].classList.remove('button__selected');
+      }
+    }
+    e.target.className += ' button__selected';
+    this.setState({
+      choosenProf: choosenProf
+    });
+  };
+
+  getAllButton = () => {
+    let buttons: HTMLCollection = document.getElementsByClassName('profbutton');
+    return buttons;
   };
 
   render() {
@@ -15,10 +38,10 @@ class YouWant extends Component<any> {
         <p>Кем ты хочешь быть?</p>
         <div className="YouWant--choose">
           <div className="choose--column">
-            <button className="button" onClick={this.chooseProf}>
+            <button className="button profbutton" onClick={this.chooseProf}>
               Аналитик
             </button>
-            <button className="button" onClick={this.chooseProf}>
+            <button className="button profbutton" onClick={this.chooseProf}>
               Разработчик
             </button>
           </div>
@@ -26,10 +49,10 @@ class YouWant extends Component<any> {
           <img className="YouWant--pic" src={lamp} alt="lamp" />
 
           <div className="choose--column">
-            <button className="button" onClick={this.chooseProf}>
+            <button className="button profbutton" onClick={this.chooseProf}>
               Тестировщик
             </button>
-            <button className="button" onClick={this.chooseProf}>
+            <button className="button profbutton" onClick={this.chooseProf}>
               Архитектор
             </button>
           </div>
