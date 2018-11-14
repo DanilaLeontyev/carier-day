64b47a052f38f4de967c3d953abce0d9c9797c1f
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-11-14 15:26:45 +0500

    Изменил контактные данные и вывод позиции

--- client/src/component/Leave.tsx
+++ client/src/component/Leave.tsx
@@ -28,37 +28,40 @@ class Leave extends Component<ILeaveProps, ILeaveState> {
   generatePosition = (result: ITask): void => {
     if (result.programmer === true) {
       this.setState({
-        position: ' как отличный Программист!'
+        position: 'Поздравляем, ты проявил себя как программист!'
       });
     }
 
     if (result.analitic === true) {
       this.setState({
-        position: ' как смышленый Аналитик!'
+        position: 'Поздравляем, ты проявил себя как аналитик!'
       });
     }
 
     if (result.testing === true) {
       this.setState({
-        position: ' как дотошный Тестер!'
+        position: 'Поздравляем, ты проявил себя как тестировщик!'
       });
     }
 
     if (result.programmer === true && result.testing === true) {
       this.setState({
-        position: ' как Программист и тестер'
+        position:
+          'Поздравляем, ты проявил себя как эксперт в области ИТ-технологий!'
       });
     }
 
     if (result.programmer === true && result.analitic === true) {
       this.setState({
-        position: ' как Программист и аналитик'
+        position:
+          'Поздравляем, ты проявил себя как эксперт в области ИТ-технологий!'
       });
     }
 
     if (result.testing === true && result.analitic === true) {
       this.setState({
-        position: ' как Тестировщик и аналитик'
+        position:
+          'Поздравляем, ты проявил себя как эксперт в области ИТ-технологий!'
       });
     }
 
@@ -68,7 +71,17 @@ class Leave extends Component<ILeaveProps, ILeaveState> {
       result.analitic === true
     ) {
       this.setState({
-        position: ' как настоящий Архитектор!'
+        position: 'Поздравляем, ты проявил себя как настоящий Архитектор!'
+      });
+    }
+
+    if (
+      result.testing === false &&
+      result.programmer === false &&
+      result.analitic === false
+    ) {
+      this.setState({
+        position: 'Благодарим за проявленный интерес!'
       });
     }
   };
@@ -81,10 +94,22 @@ class Leave extends Component<ILeaveProps, ILeaveState> {
     return (
       <div className="Leave">
         <p>
-          Поздравляем! В результате тестирования ты проявил себя
           {this.state.position} <br />
-          Мы ждем твое резюме по адресу:{' '}
-          <a href="mailto:65rcr_resume@cbr.ru">65rcr_resume@cbr.ru</a>
+          Высылай нам свое резюме, мы рассмотрим тебя в качестве потенциального
+          кандидата в нашу команду <br />
+          e-mail:{' '}
+          <a className="Leave--anchor" href="mailto:65rcr_resume@cbr.ru">
+            65rcr_resume@cbr.ru
+          </a>{' '}
+          <br />
+          тел.:{' '}
+          <a className="Leave--anchor" href="tel:+73432696797">
+            +7(343)269-69-97
+          </a>
+          ,{' '}
+          <a className="Leave--anchor" href="tel:+73432696584">
+            +7(343)269-65-87
+          </a>
         </p>
         <img className="Leave--pic" src={crown} alt="crown" />
       </div>
